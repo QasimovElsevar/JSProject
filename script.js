5896e2cef72a07b9fb5498f0af2c5263b9822424
elfuciy <elsevar.q.656@gmail.com>
2025-11-24 09:29:23 +0400

    second day

--- script.js
+++ script.js
@@ -1,55 +1,66 @@
-// Small interactions (optional)
 const input = document.querySelector('.input');
 const clearBtn = document.querySelector('.clear-btn');
 const addBtn = document.querySelector('.add-btn');
 const plus = document.querySelector('.plus');
 const todoList = document.querySelector('.todo-list');
-const sh
 
-clearBtn.addEventListener('click', () => { input.value = ''; input.focus(); });
+const listArray = [];
+
+
+clearBtn.addEventListener('click', () => { 
+  input.value = ''; 
+  input.focus(); });
+  
 addBtn.addEventListener('click', addPushed);
 plus.addEventListener('click', plusPushed)
 
-
 function addPushed() {
-  input.classList.add('become-list')
-  listArray.push(input.value)
-  console.log(listArray)
-
-   const text = input.value.trim();
+  const text = input.value.trim();
   if (!text) return;
 
-  // Add new task to list
-  const li = document.createElement('li');
-  li.textContent = text;
-  todoList.prepend(li);
+  listArray.push(text);
+  console.log(listArray);
 
-  // Hide input + clear
+  input.value = "";
+
   input.style.display = "none";
   clearBtn.style.display = "none";
-
-  // Show list
   todoList.style.display = "block";
+
+  showList();
+}
+
+function showList() {
+  todoList.innerHTML = "";
+
+  listArray.forEach((task, index) => {
+    const li = document.createElement('li');
+
+    const textSpan = document.createElement('span');
+    textSpan.textContent = task;
+
+    const img = document.createElement('img');
+     img.src = "Group 77.png";     
+    img.classList.add('clear-img')
+     
+
+    img.addEventListener('click', () => {
+      listArray.splice(index, 1); 
+      showList();              
+    });
+
+    li.appendChild(textSpan);
+    li.appendChild(img);
+
+    todoList.prepend(li);
+  });
 }
 
 function plusPushed() {
   input.classList.remove('become-list')
 
-   input.style.display = "block";
+  input.style.display = "block";
   clearBtn.style.display = "block";
 
-  // Show list
   todoList.style.display = "none";
 }
-
-
-
-// addBtn.addEventListener('click', () => {
-
-
-//   // // Delete logic
-//   // item.querySelector('.todo-delete').addEventListener('click', () => {
-//   //   item.classList.remove('show');       // fade out
-//   //   setTimeout(() => item.remove(), 250); // remove after animation
-//   // });
-// });
